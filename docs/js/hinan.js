7922be3e389385fae110c2df38468afadc2e639e
仁志 <l>
2018-09-05 08:41:23 +0900

    up

--- docs/js/hinan.js
+++ docs/js/hinan.js
@@ -102,7 +102,7 @@ function kyot(){
 }
 
 function os(){
-	document.getElementById("hinan_map").innerHTML = '<iframe width="100%" height="580" scrolling="no" frameborder="no" src="hinanmap/osakaわ.html"></iframe>';
+	document.getElementById("hinan_map").innerHTML = '<iframe width="100%" height="580" scrolling="no" frameborder="no" src="hinanmap/osaka.html"></iframe>';
 }
 
 function hg(){
