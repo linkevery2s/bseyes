d1a99aba2da47400b5fac33f72edafeaaa979cab
linkevery2s <legend1world@gmail.com>
2018-09-06 20:43:32 +0900

    up

---
+++ docs/ai/eye.js
@@ -0,0 +1,36 @@
+(function() {
+
+  var eyeui = new BotUI('search-repo');
+
+
+  //初期メッセージ
+  eyeui.message.bot({
+    content: 'こんにちは！'
+  }).then(init);
+
+
+  function init() {
+    eyeui.message.bot({
+      delay: 500,  //メッセージの表示タイミングをずらす
+      content: 'お呼びでしょうか？'
+    }).then(function() {
+
+      //キーワードの入力
+  eyeui.message.human({
+    delay: 1500,
+    content: 'こんにちは、ユーザーです！'
+  });
+ 
+  });
+  }
+
+
+
+  //プログラムを終了する処理
+  function end() {
+    eyeui.message.bot({
+      content: 'ご利用ありがとうございました！'
+    })
+  }
+
+})();
